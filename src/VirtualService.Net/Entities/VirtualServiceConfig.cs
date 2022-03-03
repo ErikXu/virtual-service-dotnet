a67dbaf43f88968d6998b0d424e30b21f26b56cc
Erik Xu <81636759@qq.com>
2022-03-03 18:26:59 +0800

    upgrade to .net 6.

--- src/VirtualService.Net/Entities/VirtualServiceConfig.cs
+++ src/VirtualService.Net/Entities/VirtualServiceConfig.cs
@@ -1,21 +1,66 @@
 ﻿using k8s.Models;
 using KubeOps.Operator.Entities;
+using System.Collections.Generic;
+using KubeOps.Operator.Entities.Annotations;
 
 namespace VirtualService.Net.Entities
 {
     [KubernetesEntity(Group = "extension.networking.istio.io", ApiVersion = "v1", Kind = "VirtualServiceConfig")]
+    [KubernetesEntityShortNames("vsc")]
     public class VirtualServiceConfig : CustomKubernetesEntity<VirtualServiceConfigSpec, VirtualServiceConfigStatus>
     {
 
     }
 
+    [Description("This is the VirtualServiceConfigSpec Description")]
     public class VirtualServiceConfigSpec
     {
+        [Required]
+        public string VirtualServiceName { get; set; } = string.Empty;
+
+        [Required]
         public string Host { get; set; } = string.Empty;
+
+        public List<HttpRoute> Http { get; set; } = new List<HttpRoute>();
     }
 
+    [Description("This is the VirtualServiceConfigStatus Description")]
     public class VirtualServiceConfigStatus
     {
         public string Status { get; set; } = string.Empty;
+    }
+
+    public class HttpRoute
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public HttpMatchRequest Match { get; set; } = new HttpMatchRequest();
+
+        public HttpRouteDestination Route { get; set; } = new HttpRouteDestination();
+    }
+
+    public class HttpMatchRequest
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public StringMatch Uri { get; set; } = new StringMatch();
+
+        public IDictionary<string, StringMatch> Headers { get; set; } = new Dictionary<string, StringMatch>();
+    }
+
+    public class HttpRouteDestination
+    {
+        public string Host { get; set; } = string.Empty;
+
+        public string Subset { get; set; } = string.Empty;
+    }
+
+    public class StringMatch
+    {
+        public string Exact { get; set; } = string.Empty;
+
+        public string Prefix { get; set; } = string.Empty;
+
+        public string Regex { get; set; } = string.Empty;
     }
 }
--- src/build.sh
+++ src/build.sh
@@ -3,5 +3,5 @@
 docker run --rm -i \
     -v /root/.nuget/packages/:/root/.nuget/packages/ \
     -v ${PWD}:/workspace \
-    mcr.microsoft.com/dotnet/sdk:5.0-alpine \
+    mcr.microsoft.com/dotnet/sdk:6.0-alpine \
     sh -c 'cd /workspace && sh publish.sh'
