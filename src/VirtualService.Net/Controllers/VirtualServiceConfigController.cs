af7f10b7d707713151b04f0cf675d4662bec21cc
Erik Xu <81636759@qq.com>
2022-03-07 17:18:26 +0800

    Apply virtual service.

--- src/VirtualService.Net/Controllers/VirtualServiceConfigController.cs
+++ src/VirtualService.Net/Controllers/VirtualServiceConfigController.cs
@@ -10,6 +10,8 @@ using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 using VirtualService.Net.Serialization;
 using System.Collections.Generic;
+using System.Linq;
+using k8s;
 
 namespace VirtualService.Net.Controllers
 {
@@ -30,12 +32,18 @@ namespace VirtualService.Net.Controllers
             _logger.LogInformation($"[{entity.Namespace()}/{entity.Name()}] reconciled.");
 
             var configs = await _client.List<VirtualServiceConfig>(entity.Namespace());
+            var preConfig = configs.FirstOrDefault(n => n.Name() == entity.Name());
+            if (preConfig != null)
+            {
+                configs.Remove(preConfig);
+            }
+
             configs.Add(entity);
 
             var virtualService = new Entities.VirtualService
             {
-                ApiVersion = "networking.istio.io/v1alpha3",
-                Kind = "VirtualService",
+                ApiVersion = VirtualServiceStatic.ApiVersion,
+                Kind = VirtualServiceStatic.Kind,
                 Metadata = new Metadata
                 {
                     Name = entity.Spec.VirtualServiceName,
@@ -51,8 +59,53 @@ namespace VirtualService.Net.Controllers
                 }
             };
 
-            var yaml = VirtualServiceToYaml(virtualService);
-            _logger.LogInformation(yaml);
+            foreach (var config in configs)
+            {
+                foreach (var h in config.Spec.Http)
+                {
+                    var http = new Http
+                    {
+                        Match = new List<Match>(),
+                        Route = new List<Route>()
+                    };
+
+                    var match = new Match
+                    {
+                        Uri = StringMatchToDic(h.Match.Uri)
+                    };
+
+                    if (h.Match.Headers != null && h.Match.Headers.Count > 0)
+                    {
+                        match.Headers = new Dictionary<string, Dictionary<string, string>>();
+                        foreach (var header in h.Match.Headers)
+                        {
+                            match.Headers[header.Key] = StringMatchToDic(header.Value);
+                        }
+                    }
+
+                    http.Match.Add(match);
+
+                    var route = new Route
+                    {
+                        Destination = new Destination
+                        {
+                            Host = h.Route.Host,
+                            Subset = !string.IsNullOrEmpty(h.Route.Subset) ? h.Route.Subset : null
+                        }
+                    };
+
+                    virtualService.Spec.Http.Add(http);
+                }
+            }
+
+            var k8sConfig = KubernetesClientConfiguration.InClusterConfig();
+            IKubernetes client = new Kubernetes(k8sConfig);
+            client.PatchNamespacedCustomObject(new V1Patch(virtualService, V1Patch.PatchType.ApplyPatch),
+                                               VirtualServiceStatic.Group,
+                                               VirtualServiceStatic.Version,
+                                               virtualService.Metadata.Namespace,
+                                               VirtualServiceStatic.Plural,
+                                               virtualService.Metadata.Name);
             return null;
         }
 
@@ -80,6 +133,20 @@ namespace VirtualService.Net.Controllers
             var yaml = serializer.Serialize(virtualService);
 
             return yaml;
+        }
+
+        private Dictionary<string, string> StringMatchToDic(StringMatch match)
+        {
+            if (!string.IsNullOrEmpty(match.Exact))
+            {
+                return new Dictionary<string, string> { { "exact", match.Exact } };
+            }
+            if (!string.IsNullOrEmpty(match.Prefix))
+            {
+                return new Dictionary<string, string> { { "prefix", match.Prefix } };
+            }
+
+            return new Dictionary<string, string> { { "regex", match.Regex } };
         }
     }
 }
---
+++ src/VirtualService.Net/Entities/VirtualServiceStatic.cs
@@ -0,0 +1,15 @@
+﻿namespace VirtualService.Net.Entities
+{
+    public class VirtualServiceStatic
+    {
+        public static string ApiVersion { get; set; } = "networking.istio.io/v1alpha3";
+
+        public static string Kind { get; set; } = "VirtualService";
+
+        public static string Group { get; set; } = "networking.istio.io";
+
+        public static string Version { get; set; } = "v1alpha3";
+
+        public static string Plural { get; set; } = "virtualservices";
+    }
+}
